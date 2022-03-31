ba652cc20a97b6581b91cd2e46140ced78c9840b
wesleywrl <wesleyramos.ieq@gmail.com>
2022-03-31 15:30:00 -0300

    test_artigos

--- projeto_teste/tb_test/test_artigos.py
+++ projeto_teste/tb_test/test_artigos.py
@@ -12,8 +12,19 @@ class TestArtigos:
         status = resposta_dict["status"]
         assert status == 'OK'
 
-    def test_retorno_api(self):
-        URL = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key={self.api_key}'
-        resposta = requests.get(url=URL)
-        return resposta.json()
-        assert true
+    def test_tag_query(self):
+        URL = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?tag_query("Obama", max_results=2)&api-key={self.api_key}'
+        assert 2, len(URL)
+
+    def test_article_search(self):
+        URL = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?article_search("Joe Biden", results=80)&api-key={self.api_key}'
+        assert 80, len(URL)
+        for article in URL:
+            assert article, dict
+    def test_article_metada(self):
+        URL = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?.article_metadata("https://www.nytimes.com/live/2021/02/10/us/impeachment-trial/prosecutors-begin-arguments-against-trump-saying-he-became-the-inciter-in-chief-of-a-dangerous-insurrection")&api-key={self.api_key}'
+        assert URL, list
+        for article in URL:
+            assert article, dict
+
+
